a8e21097c9b56e1664236b34545e6b3642a35fd6
nikolay-pavlnk <pavlenkokolya421@gmail.com>
2019-04-14 00:27:25 +0300

    messenger_release

--- bot.py
+++ bot.py
@@ -22,13 +22,13 @@ def receive_message():
         for event in output['entry']:
             messaging = event['messaging']
             for message in messaging:
-            	print(message)
-                if message.get('message'):
+				print(message)
+				if message.get('message'):
                 #Facebook Messenger ID for user so we know where to send response back to
-                    recipient_id = message['sender']['id']
-                if message['message'].get('text'):
-                    response_sent_text = get_message(message['message'].get('text'))
-                    send_message(recipient_id, response_sent_text)
+                	recipient_id = message['sender']['id']
+				if message['message'].get('text'):
+					response_sent_text = get_message(message['message'].get('text'))
+					send_message(recipient_id, response_sent_text)
 
     return "Message Processed"
 
