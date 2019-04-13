48e7289c0060928de1039236e8d16c91139dc552
nikolay-pavlnk <pavlenkokolya421@gmail.com>
2019-04-14 00:33:20 +0300

    messenger_release2

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ def receive_message():
 				print(message)
 				if message.get('message'):
                 #Facebook Messenger ID for user so we know where to send response back to
-                	recipient_id = message['sender']['id']
+					recipient_id = message['sender']['id']
 				if message['message'].get('text'):
 					response_sent_text = get_message(message['message'].get('text'))
 					send_message(recipient_id, response_sent_text)
