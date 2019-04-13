db248993632b261e73cd7182c8e1e24fe7a07964
nikolay-pavlnk <pavlenkokolya421@gmail.com>
2019-04-13 23:59:24 +0300

    messenger

--- bot.py
+++ bot.py
@@ -3,11 +3,8 @@ from flask import Flask, request
 from pymessenger.bot import Bot
 
 app = Flask(__name__)
-ACCESS_TOKEN = 'EAAILZAP8AbQwBAKj2kINB7EOwPucUCooZBSx0kxZBnZC2xyZACGcwpBzR5ZA7lCblUt5GcGB1bCqzvXx0IjJnVkcrffgaQ7MOtZA8morgBKf1hto0VCdBq5KXOclVrRMPgLDzu27CdEJVGjpZCcZBwcWOql71Fa4VTkZC0ZCFzKm6NRagZDZD'
-VERIFY_TOKEN = 'BotTestMessenger'
 bot = Bot(ACCESS_TOKEN)
 
-#We will receive messages that Facebook sends our bot at this endpoint 
 @app.route("/", methods=['GET', 'POST'])
 def receive_message():
     if request.method == 'GET':
@@ -17,7 +14,6 @@ def receive_message():
         return verify_fb_token(token_sent)
     #if the request was not get, it must be POST and we can just proceed with sending a message back to user
     else:
-        # get whatever message a user sent the bot
         output = request.get_json()
         for event in output['entry']:
             messaging = event['messaging']
@@ -26,30 +22,25 @@ def receive_message():
                 #Facebook Messenger ID for user so we know where to send response back to
                     recipient_id = message['sender']['id']
                 if message['message'].get('text'):
-                    response_sent_text = get_message()
+                    response_sent_text = get_message(message['message'].get('text'))
                     send_message(recipient_id, response_sent_text)
-                #if user sends us a GIF, photo,video, or any other non-text item
-                if message['message'].get('attachments'):
-                    response_sent_nontext = get_message()
-                    send_message(recipient_id, response_sent_nontext)
     return "Message Processed"
 
 
 def verify_fb_token(token_sent):
-    #take token sent by facebook and verify it matches the verify token you sent
-    #if they match, allow the request, else return an error 
     if token_sent == VERIFY_TOKEN:
         return request.args.get("hub.challenge")
     return 'Invalid verification token'
 
-
-#chooses a random message to send to the user
 def get_message():
-    sample_responses = ["You are stunning!", "We're proud of you.", "Keep on being you!", "We're greatful to know you :)"]
-    # return selected item to the user
-    return random.choice(sample_responses)
+	greetings = [True if word.lower() in GREETING_INPUTS else False for word in message.text.split()]
+    
+	if True in greetings:
+		bot.send_message(message.chat.id, np.random.choice(GREETING_RESPONSES))
+	else:
+		bot.send_message(message.chat.id, get_response(message.text))
+		corpus.remove(message.text)
 
-#uses PyMessenger to send response to user
 def send_message(recipient_id, response):
     #sends user the text message provided via input response parameter
     bot.send_text_message(recipient_id, response)
---
+++ config.py
@@ -0,0 +1,5 @@
+ACCESS_TOKEN = 'EAAILZAP8AbQwBAKj2kINB7EOwPucUCooZBSx0kxZBnZC2xyZACGcwpBzR5ZA7lCblUt5GcGB1bCqzvXx0IjJnVkcrffgaQ7MOtZA8morgBKf1hto0VCdBq5KXOclVrRMPgLDzu27CdEJVGjpZCcZBwcWOql71Fa4VTkZC0ZCFzKm6NRagZDZD'
+VERIFY_TOKEN = 'BotTestMessenger'
+# Tokens should be set as os.environ, but i will not do it with my os
+GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up","hey",)
+GREETING_RESPONSES = ["hi", "hey", "*nods*", "hi there", "hello", "I am glad! You are talking to me"]
